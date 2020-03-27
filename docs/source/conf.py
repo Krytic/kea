5b95df41aa3044837134505dca07ff7ffeef087d
Max Briel <max.briel@auckland.ac.nz>
2020-03-28 00:07:52 +1300

    typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-if on_rt:
+if on_rtd:
     sus.path.insert(0, os.path.abspath('../../scripts'))
 
 # -- Project information -----------------------------------------------------
