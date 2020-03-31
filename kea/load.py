c717f379ca8e6493e00281b367303933714d576d
Max Briel <max.briel@auckland.ac.nz>
2020-03-31 17:21:03 +1300

    Adds a contants submodule

--- kea/load.py
+++ kea/load.py
@@ -6,8 +6,10 @@ import gzip
 import os
 from hoki import load
 import kea.hist
+from kea.constants import *
 import pandas as pd
 import numpy as np
+import pickle
 
 def gunzip(source_filepath, dest_filepath, block_size=65536):
     """Unpacks a zipped file.
@@ -111,7 +113,7 @@ def loadGW(file, types):
     """
     data = pd.read_csv(file,
                     sep= "\s+",
-                    names=["log_age", "BHBH", "BHNS", "NSNS", "age_yrs"],
+                    names=["log_age", "NSNS", "BHNS", "BHBH", "age_yrs"],
                     engine="python")
     rates = {i:kea.hist.BPASS_hist() for i in types}
     val = list(rates.values())[0]
@@ -121,15 +123,49 @@ def loadGW(file, types):
 
     # normalise the rates to #Events/yr/M_sun
     bin_widths = np.array([val.getBinWidth(i)*1e9 for i in range(0, val.getNBins())])
-    for i in rates:
-        rates[i] = rates[i]/1e6/bin_widths
+    if "v2.2bray" in file:
+        for i in rates:
+            rates[i] = rates[i]/1e6/bin_widths
+    elif "v2.1hobbs" in file:
+        for i in rates:
+            rates[i] = rates[i]/bin_widths
+    elif "v2.2hobbs" in file:
+        for i in rates:
+            rates[i] = rates[i]/1e6/bin_widths
+    else:
+        print("NO Normalisation applied")
+    return rates
+
+
+def loadAllGW(folder):
+    """Loads all the GW event metallicities in the given folder.
+
+    Parameters
+    ----------
+    folder : str
+        FOlder containing the GW events
+
+    Returns
+    -------
+    dict
+        A dictionary containing per metallicity the BPASS rates per event type.
+
+    """
+    compact_types = ["BHBH", "BHNS", "NSNS"]
+    num_Z = BPASS_NUM_METALLICITIES
+
+    rates = {}
+    for x, i in enumerate(BPASS_METALLICITIES):
+        rates[num_Z[x]] = loadGW(folder+"gwmergerdata.z"+i+".dat", compact_types)
 
     return rates
 
 
 def loadAllRates(data_folder):
     """ Loads the SNe & compact merger rates for all available
-    metallicities in BPASS
+    metallicities in BPASS.
+
+    Loads v2.2 Hobbs for the Gravitational Wave events.
 
     Parameters
     ----------
@@ -144,13 +180,77 @@ def loadAllRates(data_folder):
 
         The event rate are in #events/yr/:math:`M_\odot`.
     """
-    metallicities = ["em5", "em4", "001","002", "003", "004", "006", "008", "010", "014", "020", "030", "040"]
     SNe_types = ["ccsn", "Ia", "LGRB", "PISNe"]
     compact_types = ["BHBH", "BHNS", "NSNS"]
+    num_Z = BPASS_NUM_METALLICITIES
 
     rates = {}
-    for i in metallicities:
-        rates[i] = loadBPASS(data_folder+"bpass_v2.2.1_imf135_300/supernova-bin-imf135_300.z"+i+".dat", SNe_types)
-        rates[i].update(loadGW(data_folder+"GWrates/v2.2hobbs/gwmergerdata.z"+i+".dat", compact_types))
+    for x, i in enumerate(BPASS_METALLICITIES):
+        rates[num_Z[x]] = loadBPASS(data_folder+"bpass_v2.2.1_imf135_300/supernova-bin-imf135_300.z"+i+".dat", SNe_types)
+        rates[num_Z[x]].update(loadGW(data_folder+"GWrates/v2.2hobbs/gwmergerdata.z"+i+".dat", compact_types))
 
     return rates
+
+
+def calculate2dSFRD(data_file, time_file):
+    """Extracts the Stellar Formation Rate Density from the Milenium Simulation
+    and puts it into a 2D grid of time and metallicity (pandas DataFrame).
+
+    Parameters
+    ----------
+    data_file : str
+        The filename of the cosmological simulation.
+    time_file : str
+        The filename of the time relation in the cosmological simulation.
+
+    Returns
+    -------
+    pandas DataFrame
+        A 2D Stellar Formation Rate Density over snapshot number and metallicity.
+    """
+    # edges are defined by BPASS metallicity binning
+    edges = [0.00005, 0.0005, 0.0015, 0.0025, 0.0035, 0.005, 0.007, 0.009, 0.012, 0.017, 0.025, 0.035]
+    text_metals = ["em5", "em4", "001","002", "003", "004", "006", "008", "010", "014", "020", "030", "040"]
+    num_metals = [0.00001, 0.0001, 0.001, 0.002, 0.003, 0.004, 0.006, 0.008, 0.010, 0.014, 0.020, 0.030, 0.040]
+
+    data = pd.read_csv(data_file, comment="#")
+    tr = pd.read_csv(time_file, comment="#")
+
+    SFR = pd.DataFrame(0.0, columns=num_metals, index=tr["snapNum"])
+
+    for index, row in data.iterrows():
+        snapnum = row["snapnum"]
+        stellarMass = row["stellarMass"]  #both in 10**10 solar masses
+        if stellarMass == 0.0:
+            continue
+        metallicity = row["metalsStellarMass"]/stellarMass
+
+        sfr = row["sfr"]
+        location = np.where(metallicity <= edges)[0]
+        if len(location) == 0:
+            Zbin = num_metals[0]
+        else:
+            Zbin = num_metals[location[0]]
+        SFR.at[int(snapnum), Zbin] += sfr
+
+    SFR = SFR/((62.5/0.73)**3)  #Msun/yr/Mpc3
+    return SFR
+
+
+def load2dSFRD(file):
+    """Loads the 2d Stellar Formation Rate Density from a pickle file.
+
+    Parameters
+    ----------
+    file : str
+        The filename of the pickled 2d SFRD
+
+    Returns
+    -------
+    pandas DataFrame
+        An unpickled pandas DataFrame containing the Stellar Formation Rate
+        Density over snapshot number of the Millenium Simulation and metallicity
+        of BPASS.
+    """
+    SFRS = pickle.load(open(file, 'rb'))
+    return SFRS
